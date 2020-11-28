ce3e76ef837a897477b675a6e94e0e66616d4719
Jamee Kim <jameekim@nodaplife.me>
2020-11-27 23:31:25 -0500

    Add feature to show the leading team (1st place)

--- server/models/tournament.js
+++ server/models/tournament.js
@@ -20,27 +20,32 @@ let tournamentModel = mongoose.Schema({
 });
 
 /**
- * Instance method for Tournament model
+ * Instance method for Tournament model: Get team names in semi-final brackets
  *
  * This is indented to be used by tournament models that are fetched by `getActiveTournamentList` controller method.
  */
 tournamentModel.methods.getTop4Teams = function () {
     const teamNames = ["", "", "", ""];
 
+    // final bracket is not found
     if (!this.finalBracket) {
         return teamNames;
     }
 
+    // there are only 2 teams in this tournament
     if (this.teams.length === 2) {
         teamNames[0] = this.teams[0].name;
         teamNames[1] = this.teams[1].name;
         return teamNames;
     }
 
+    // get the index of the teams in the semi-final brackets
     const idx1 = this.finalBracket.children[0].team1;
     const idx2 = this.finalBracket.children[0].team2;
     const idx3 = this.finalBracket.children[1].team1;
     const idx4 = this.finalBracket.children[1].team2;
+
+    // populate team names if the indices are valid
 
     if (idx1 >= 0) {
         teamNames[0] = this.teams[idx1].name;
@@ -60,5 +65,53 @@ tournamentModel.methods.getTop4Teams = function () {
 
     return teamNames;
 }
+
+/**
+ * Instance method for Tournament model: Get the leading team if available
+ *
+ * This is indented to be used by tournament models that are fetched by `getActiveTournamentList` controller method.
+ *
+ * @returns Object with following fields
+ *          - status: 'decided' | 'leading' | 'onPar' | 'notStarted'
+ *          - topTeam: string
+ *                     will be a single team name if 'decided' or 'leading'
+ *                     will be two team names separated by ',' if 'onPar'
+ *                     will be a single team name or empty string if 'notStarted'
+ */
+tournamentModel.methods.getTopTeam = function () {
+    const result = { status: 'notStarted', topTeam: '' };
+
+    // only one team is on final or no team is assigned to the final
+    if (this.finalBracket.team1 < 0 || this.finalBracket.team2 < 0) {
+        if (this.finalBracket.team1 >= 0) {
+            result.topTeam = this.teams[this.finalBracket.team1].name;
+        } else if (this.finalBracket.team2 >= 0) {
+            result.topTeam = this.teams[this.finalBracket.team2].name;
+        }
+        return result;
+    }
+
+    const team1Name = this.teams[this.finalBracket.team1].name;
+    const team2Name = this.teams[this.finalBracket.team2].name;
+
+    // the final is already finished
+    if (typeof this.finalBracket.isFirstWon === 'boolean') {
+        result.status = 'decided';
+        result.topTeam = this.finalBracket.isFirstWon ? team1Name : team2Name;
+        return result;
+    }
+
+    // both teams have the same score
+    if (this.finalBracket.score1 === this.finalBracket.score2) {
+        result.status = 'onPar';
+        result.topTeam = `${team1Name},${team2Name}`;
+        return result;
+    }
+
+    // one team is winning
+    result.status = 'leading';
+    result.topTeam = this.finalBracket.score1 > this.finalBracket.score2 ? team1Name : team2Name;
+    return result;
+};
 
 module.exports = mongoose.model('Tournament', tournamentModel, 'tournaments');
